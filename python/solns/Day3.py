b0dc6c080eb7186c53836626470ff30e1b42a31e
Kathleen West <2509480w@student.gla.ac.uk>
2020-12-09 20:39:29 +0000

    Day 3 - Part 1 + 2 Solutions

--- python/solns/Day3.py
+++ python/solns/Day3.py
@@ -23,8 +23,8 @@ rows_step = 1
 trees = 0
 
 while row < no_of_rows:
-    if (col + cols_step) >= no_of_cols:
-        col = (col + cols_step) - no_of_cols
+    if col >= no_of_cols:
+        col = col % no_of_cols
 
     if data[row][col] == '#':
         trees += 1
@@ -32,4 +32,35 @@ while row < no_of_rows:
     row += rows_step
     col += cols_step
 
-print('trees: ' + str(trees))
+print('Day 3 Part 1: ' + str(trees))
+
+
+# Part Two
+slopes = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]
+tree_entries = []
+
+for slope in slopes:
+    tree_count = 0
+    cols_step = slope[0]
+    rows_step = slope[1]
+    row = 0
+    col = 0
+
+    while row < no_of_rows:
+        if col >= no_of_cols:
+            col = col % no_of_cols
+
+        if data[row][col] == '#':
+            tree_count += 1
+
+        row += rows_step
+        col += cols_step
+
+    tree_entries.append(tree_count)
+
+result = 1
+
+for tree_count in tree_entries:
+    result *= tree_count
+
+print('Day 3 Part 2: ' + str(result))
