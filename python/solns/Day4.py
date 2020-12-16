27d97d41af9a58f38909a94dcfd8147b1c2bc769
Kathleen West <2509480w@student.gla.ac.uk>
2020-12-16 21:34:45 +0000

    Day 4 - Part 1 & 2

--- python/solns/Day4.py
+++ python/solns/Day4.py
@@ -1,6 +1,6 @@
 # Imports
 from python.util.Utility import read_rows
-
+import re
 
 # Day 4 of Advent of Code
 
@@ -9,53 +9,90 @@ file = '../../data/4.input'
 
 # Retrieve Data from File
 data = read_rows(file)
+required_props = ['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid']
+
 
 # Part One
 
+# Read Passports
+passports = []
+pass_index = 0
 
-# You arrive at the airport only to realize that you grabbed your North Pole Credentials
-# instead of your passport. While these documents are extremely similar, North Pole
-# Credentials aren't issued by a country and therefore aren't actually valid documentation
-# for travel in most of the world.
-# It seems like you're not the only one having problems, though; a very long line has
-# formed for the automatic passport scanners, and the delay could upset your travel itinerary.
-# Due to some questionable network security, you realize you might be able to solve both of
-# these problems at the same time.
-# The automatic passport scanners are slow because they're having trouble detecting which
-# passports have all required fields. The expected fields are as follows:
-# byr (Birth Year)
-# iyr (Issue Year)
-# eyr (Expiration Year)
-# hgt (Height)
-# hcl (Hair Color)
-# ecl (Eye Color)
-# pid (Passport ID)
-# cid (Country ID)
-# Passport data is validated in batch files (your puzzle input). Each passport is represented
-# as a sequence of key:value pairs separated by spaces or newlines. Passports are separated
-# by blank lines.
-# Here is an example batch file containing four passports:
-#
-# ecl:gry pid:860033327 eyr:2020 hcl:#fffffd
-# byr:1937 iyr:2017 cid:147 hgt:183cm
-#
-# iyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884
-# hcl:#cfa07d byr:1929
-#
-# hcl:#ae17e1 iyr:2013
-# eyr:2024
-# ecl:brn pid:760753108 byr:1931
-# hgt:179cm
-#
-# hcl:#cfa07d eyr:2025 pid:166559648
-# iyr:2011 ecl:brn hgt:59in
-# The first passport is valid - all eight fields are present. The second passport is
-# invalid - it is missing hgt (the Height field).
-# The third passport is interesting; the only missing field is cid, so it looks like data
-# from North Pole Credentials, not a passport at all! Surely, nobody would mind if you made
-# the system temporarily ignore missing cid fields. Treat this "passport" as valid.
-# The fourth passport is missing two fields, cid and byr. Missing cid is fine, but missing
-# any other field is not, so this passport is invalid.
-# According to the above rules, your improved system would report 2 valid passports.
-# Count the number of valid passports - those that have all required fields. Treat cid as
-# optional. In your batch file, how many passports are valid?
+for entry in data:
+    if pass_index >= len(passports):
+        passports.append({})
+
+    if entry != '':
+        properties = entry.split(' ')
+
+        for prop in properties:
+            key = prop.split(':')[0]
+            value = prop.split(':')[1]
+
+            passports[pass_index][key] = value
+    else:
+        pass_index += 1
+
+# Count Number of Valid Passports
+valid_passports = 0
+
+for passport in passports:
+    valid = True
+
+    for field in required_props:
+        if field not in passport:
+            valid = False
+
+    if valid:
+        valid_passports += 1
+
+# Day 4 Part 1
+print("Day 4 Part 1: " + str(valid_passports))
+
+
+# Part Two
+def validate_passport(some_passport):
+    is_valid = True
+
+    if len(some_passport) < len(required_props):
+        return False
+
+    for p in required_props:
+        if p not in some_passport:
+            return False
+
+    if not (1920 <= int(some_passport['byr']) <= 2002):
+        is_valid = False
+    if not (2010 <= int(some_passport['iyr']) <= 2020):
+        is_valid = False
+    if not (2020 <= int(some_passport['eyr']) <= 2030):
+        is_valid = False
+
+    if 'cm' in some_passport['hgt'] and not (150 <= int(some_passport['hgt'][:-2]) <= 193):
+        is_valid = False
+    elif 'in' in some_passport['hgt'] and not (59 <= int(some_passport['hgt'][:-2]) <= 76):
+        is_valid = False
+
+    if 'cm' not in some_passport['hgt'] and 'in' not in some_passport['hgt']:
+        is_valid = False
+
+    if some_passport['ecl'] not in ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth']:
+        is_valid = False
+    if re.match(r'^#[0-9a-f]{6}$', some_passport['hcl']) is None:
+        is_valid = False
+    if re.match(r'^\d{9}$', some_passport['pid']) is None:
+        is_valid = False
+
+    return is_valid
+
+
+valid_passports = 0
+
+for passport in passports:
+    valid = validate_passport(passport)
+
+    if valid:
+        valid_passports += 1
+
+# Day 4 Part 2
+print("Day 4 Part 2: " + str(valid_passports))
