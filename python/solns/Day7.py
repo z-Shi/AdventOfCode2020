4893831a97386af9d234267f76498eab8501fa33
z-Shi <zshi767@gmail.com>
2020-12-19 20:08:25 +0000

    Preparation for Day 7

---
+++ python/solns/Day7.py
@@ -0,0 +1,11 @@
+# Imports
+from python.util.Utility import read_rows
+
+# Day 7 of Advent of Code
+
+# Data for Day 7
+file = '../../data/7.input'
+
+# Retrieve Data from File
+data = read_rows(file)
+
