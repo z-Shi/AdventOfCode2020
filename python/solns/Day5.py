3440c3c5743ed916f4e902c3d6e100aad207c573
z-Shi <zshi767@gmail.com>
2020-12-19 18:29:44 +0000

    Day 5 Part 1

--- python/solns/Day5.py
+++ python/solns/Day5.py
@@ -1,12 +1,28 @@
 # Imports
-
+from python.util.Utility import read_rows, binary_to_decimal
 
 # Day 5 of Advent of Code
 
 # Data for Day 5
-file = '../../5.input'
+file = '../../data/5.input'
 
 # Retrieve Data from File
-
+data = read_rows(file)
 
 # Part One
+maximum_seat_id = 0
+
+for entry in data:
+    row_section = entry[:-3:].replace('F', '0').replace('B', '1')
+    seat_section = entry[-3::].replace('L', '0').replace('R', '1')
+
+    row = binary_to_decimal(row_section)
+    seat = binary_to_decimal(seat_section)
+
+    seat_id = (row * 8) + seat
+
+    if seat_id > maximum_seat_id:
+        maximum_seat_id = seat_id
+
+# Day 5 Part 1
+print('Day 5 Part 1: ' + str(maximum_seat_id))
--- python/util/Utility.py
+++ python/util/Utility.py
@@ -9,3 +9,15 @@ def read_integers(filename):
 def read_rows(filename):
     with open(filename) as f:
         return [line.strip() for line in f]
+
+
+def binary_to_decimal(binary):
+    decimal_value = 0
+    base = 1
+
+    for index in range(len(binary) - 1, -1, -1):
+        if binary[index] == '1':
+            decimal_value += base
+        base *= 2
+
+    return decimal_value
