65c121cd0eac9fafd09612c16685a4b18832e1ca
z-Shi <zshi767@gmail.com>
2021-01-03 13:22:40 +0000

    Day 12 Preparation

---
+++ python/solns/Day12.py
@@ -0,0 +1,10 @@
+# Imports
+from python.util.Utility import read_rows
+
+# Day 12 of Advent of Code
+
+# Data for Day 12
+file = '../../data/12.input'
+
+# Retrieve Data from File
+data = read_rows(file)
