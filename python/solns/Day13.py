2ea9c6120cc4a67cd5de76a8f34d908796a73711
z-Shi <zshi767@gmail.com>
2021-01-03 19:01:39 +0000

    Day 13 Part 1 Solution

--- python/solns/Day13.py
+++ python/solns/Day13.py
@@ -8,3 +8,47 @@ file = '../../data/13.input'
 
 # Retrieve Data from File
 data = read_rows(file)
+
+
+# Part One Functions
+def parse_data(input_data):
+    earliest_timestamp = int(input_data[0].strip())
+    bus_list = []
+
+    for entry in input_data[1].split(','):
+        if entry != 'x':
+            bus_list.append(int(entry))
+
+    return earliest_timestamp, bus_list
+
+
+def find_earliest_departure_time(earliest_timestamp, bus_list):
+    departure_time = None
+    bus_id = None
+    found = False
+    time = earliest_timestamp
+
+    while not found:
+        for bus in bus_list:
+            if time % bus == 0:
+                departure_time = time
+                bus_id = bus
+                found = True
+
+        time += 1
+
+    return departure_time, bus_id
+
+
+def part_one(earliest_timestamp, timestamp, bus_id):
+    time_diff = timestamp - earliest_timestamp
+    return bus_id * time_diff
+
+
+# Part One
+earliest_departure_time, buses = parse_data(data)
+actual_departure_time, actual_bus = find_earliest_departure_time(earliest_departure_time, buses)
+result = part_one(earliest_departure_time, actual_departure_time, actual_bus)
+
+# Day 13 Part 1
+print(f'Day 13 Part 1: {result}')
